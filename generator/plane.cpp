ddd3dbcefe7186862782ffa823854b08c04341f1
brazafonso <61985783+brazafonso@users.noreply.github.com>
2022-03-09 19:33:32 +0000

    inicio da hash e lista para guardar pontos e indices

--- generator/plane.cpp
+++ generator/plane.cpp
@@ -1,43 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <map>
+#include <list>
 #include "plane.h"
+#include "../Utilities/point.cpp"
+#include <iostream>
 
 int write_plane(float size, int div, char* fname) {
-    FILE* file = fopen(fname,"w+");
-    float x, z;
+    if (size > 0 && div > 0) {
+        std::map<size_t,point> pointsHash;
+        list<size_t> indexList;
 
-    x = -size / 2;
-    z = size / 2;
+        FILE* file = fopen(fname, "w+");
+        float x, z;
 
-    float initz = z;
+        x = -size / 2;
+        z = size / 2;
 
-    float part = size / div;
+        float initz = z;
+        float part = size / div;
 
-    int i1 = 0;
+        int i1 = 0;
 
-    while (i1 < div) {
-        int i2 = 0;
-        while (i2 < div) {
-            float ax = x + part;
-            float az = z - part;
-            write_glTriangle(file); //para teste, tirar depois;
-            write_point(x, 0, az, file);
-            write_point(x, 0, z, file);
-            write_point(ax, 0, z, file);
-            write_glEnd(file); //para teste, tirar depois;
-            write_glTriangle(file); //para teste, tirar depois;
-            write_point(ax, 0, z, file);
-            write_point(ax, 0, az, file);
-            write_point(x, 0, az, file);
-            write_glEnd(file); //para teste, tirar depois;
-            i2 = i2 + 1;
-            z = az;
+        while (i1 < div) {
+            int i2 = 0;
+            while (i2 < div) {
+                float ax = x + part;
+                float az = z - part;
+                
+                point p1 = point(x, 0, az);
+                pointsHash.insert(std::make_pair(p1.get_hash_code(), p1));
+                indexList.push_back(p1.get_hash_code());
+
+                point p2 = point(x, 0, z);
+                pointsHash.insert(std::make_pair(p2.get_hash_code(), p2));
+                indexList.push_back(p2.get_hash_code());
+
+                point p3 = point(ax, 0, z);
+                pointsHash.insert(std::make_pair(p3.get_hash_code(), p3));
+                indexList.push_back(p3.get_hash_code());
+
+
+                point p4 = point(ax, 0, z);
+                pointsHash.insert(std::make_pair(p4.get_hash_code(), p4));
+                indexList.push_back(p4.get_hash_code());
+
+                point p5 = point(ax, 0, az);
+                pointsHash.insert(std::make_pair(p5.get_hash_code(), p5));
+                indexList.push_back(p5.get_hash_code());
+
+                point p6 = point(x, 0, az);
+                pointsHash.insert(std::make_pair(p6.get_hash_code(), p6));
+                indexList.push_back(p6.get_hash_code());
+
+                i2 = i2 + 1;
+                z = az;
+            }
+            x = x + part;
+            z = initz;
+            i1 = i1 + 1;
         }
-        x = x + part;
-        z = initz;
-        i1 = i1 + 1;
-    }
+        fclose(file);
+        /*
+        for (auto & x : pointsHash)
+        {
+            std::cout << x.first  // string (key)
+                << ':'
+                << x.second.getX() // string's value 
+                << std::endl;
+        }
 
-    fclose(file);
+        for (auto& x : indexList)
+        {
+            std::cout << x
+                << std::endl;
+        }
+        */
+    }
     return 0;
 }
