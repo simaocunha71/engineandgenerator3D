73418e582f2b1e66c008ea9966bfe3bdbaae10f6
Tiago Silva <a93277@alunos.uminho.pt>
2022-03-09 19:12:18 +0000

    update point.cpp

--- Utilities/point.cpp
+++ Utilities/point.cpp
@@ -3,22 +3,37 @@
 #include <functional>
 using namespace std;
 
-class Point {
+class point {
 private:
 	float x;
 	float y;
 	float z;
 public:
-	Point(float x, float y, float z) {
+	point(float x, float y, float z) {
 		this->x = x;
 		this->y = y;
 		this->z = z;
 	}
 
-	Point() {
+	point() {
 		this->x = 0.0f;
 		this->y = 0.0f;
 		this->z = 0.0f;
+	}
+
+	point(string point) : point() {
+		int space_one = 0;
+		int space_two = 0;
+		while (point.at(space_one) != ' ') {
+			space_one++; space_two++;
+		}
+		space_two++;
+		while (point.at(space_two++) != ' ') {
+			space_two++;
+		}
+		this->x = stof(point.substr(0, space_one));
+		this->y = stof(point.substr(space_one + 1, space_two));
+		this->z = stof(point.substr(space_two + 1, point.length()));
 	}
 
 	void setX(float x) {
