ca97c6f091611e5cfcf528140fc983c9f50cf7af
Tiago Silva <a93277@alunos.uminho.pt>
2022-04-04 23:14:56 +0100

    fase 2

--- Utilities/transformation.cpp
+++ Utilities/transformation.cpp
@@ -13,7 +13,6 @@ public:
     float x = 0;
     float y = 0;
     float z = 0;
-    bool have = false;
 
     virtual void transform() = 0;
 };
@@ -24,26 +23,9 @@ public:
         this->x = x;
         this->y = y;
         this->z = z;
-        this->have = true;
     }
     void transform() {
-        if (this->have) {
             glTranslatef(this->x, this->y, this->z);
-        }
-    }
-};
-
-class scaling : public transformation{
-public:
-    scaling(float x, float y, float z) {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-        this->have = true;
-    }
-    void transform() {
-        if (this->have) 
-            glScalef(this->x, this->y, this->z);
     }
 };
 
@@ -54,10 +36,20 @@ public:
         this->y = y;
         this->z = z;
         this->angle = angle;
-        this->have = true;
     }
     void transform() {
-        if (this->have) 
             glRotatef(this->angle, this->x, this->y, this->z);
+    }
+};
+
+class scaling : public transformation{
+public:
+    scaling(float x, float y, float z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    void transform() {
+            glScalef(this->x, this->y, this->z);
     }
 };
