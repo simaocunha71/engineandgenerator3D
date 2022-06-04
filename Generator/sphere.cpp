5ca1cb1f260049444f266a212ebbe5cf7c44c0ab
Tiago Silva <a93277@alunos.uminho.pt>
2022-06-04 02:50:36 +0100

    começo da textura da esfera

--- Generator/sphere.cpp
+++ Generator/sphere.cpp
@@ -22,7 +22,7 @@ int write_sphere(float radius, int slices, int stacks, char* fname) {
         i_slices++;
 
        
-        //stack superior
+        //stack inferior
         float alpha_1 = alpha * (i_slices-1);
         float alpha_2 = alpha * (i_slices);
         float alpha_y = -M_PI / 2 + alphay;
@@ -31,15 +31,21 @@ int write_sphere(float radius, int slices, int stacks, char* fname) {
         float x_base1 = cos(alpha_1) * (up_radius);
         float z_base1 = sin(alpha_1) * (up_radius);
 
+        float tx_1 = ((float)i_slices - 1.0f) / slices;
+
         float x_base2 = cos(alpha_2) * up_radius;
         float z_base2 = sin(alpha_2) * up_radius;
+
+        float tx_2 = (float)i_slices / slices;
+
+        float ty = 1.0f / stacks;
 
         float y = sin(alpha_y) * (radius);
         
         
-        point p1 = point(x_base1, y, z_base1, x_base1, y, z_base1);
-        point p2 = point(x_base2, y, z_base2, x_base2, y, z_base2);
-        point p3 = point(0.0f, -radius, 0.0f, 0.0f, -radius, 0.0f);
+        point p1 = point(x_base1, y, z_base1, x_base1, y, z_base1, tx_1, ty);
+        point p2 = point(x_base2, y, z_base2, x_base2, y, z_base2, tx_2, ty);
+        point p3 = point(0.0f, -radius, 0.0f, 0.0f, -radius, 0.0f,0,0);
         ps.add_point(p1);
         ps.add_point(p2);
         ps.add_point(p3);
@@ -59,6 +65,10 @@ int write_sphere(float radius, int slices, int stacks, char* fname) {
             float x_face2 = cos(alpha_2) * l_radius;
             float z_face2 = sin(alpha_2) * l_radius;
 
+            float tx_1 = ((float)i_slices - 1.0f) / slices;
+            float tx_2 = (float)i_slices / slices;
+            float ty = ((float)j - 1) / stacks;
+
             float y_face1 = sin(alpha_y_l) * (radius);
 
             //Vertices da parte de cima do quadrilatero
@@ -68,20 +78,25 @@ int write_sphere(float radius, int slices, int stacks, char* fname) {
             float x_face4 = cos(alpha_2) * up_radius;
             float z_face4 = sin(alpha_2) * up_radius;
 
+            float tx_3 = ((float)i_slices - 1.0f) / slices;
+            float tx_4 = (float)i_slices / slices;
+            
+
             float y_face2 = sin(alpha_y_up) * (radius);
+            float ty_2 = (float)j / stacks;
 
             //3 4 1
-            p1 = point(x_face1, y_face1, z_face1, x_face1, y_face1, z_face1);
-            p2 = point(x_face4, y_face2, z_face4, x_face4, y_face2, z_face4);
-            p3 = point(x_face2, y_face1, z_face2, x_face2, y_face1, z_face2);
+            p1 = point(x_face1, y_face1, z_face1, x_face1, y_face1, z_face1,tx_1,ty);
+            p2 = point(x_face4, y_face2, z_face4, x_face4, y_face2, z_face4,tx_4,ty_2);
+            p3 = point(x_face2, y_face1, z_face2, x_face2, y_face1, z_face2,tx_2,ty);
             ps.add_point(p1);
             ps.add_point(p2);
             ps.add_point(p3);
 
             //2 1 4
-            p1 = point(x_face4, y_face2, z_face4, x_face4, y_face2, z_face4);
-            p2 = point(x_face1, y_face1, z_face1, x_face1, y_face1, z_face1);
-            p3 = point(x_face3, y_face2, z_face3, x_face3, y_face2, z_face3);
+            p1 = point(x_face4, y_face2, z_face4, x_face4, y_face2, z_face4, tx_4, ty_2);
+            p2 = point(x_face1, y_face1, z_face1, x_face1, y_face1, z_face1, tx_1, ty);
+            p3 = point(x_face3, y_face2, z_face3, x_face3, y_face2, z_face3, tx_3, ty_2);
             ps.add_point(p1);
             ps.add_point(p2);
             ps.add_point(p3);
@@ -95,17 +110,24 @@ int write_sphere(float radius, int slices, int stacks, char* fname) {
         
 
         x_base1 = cos(alpha_1) * (l_radius);
-        z_base1 = sin(alpha_1) * (l_radius);
+        z_base1 = sin(alpha_1) * (l_radius); 
+
+        tx_1 = ((float)i_slices - 1.0f) / slices;
 
         x_base2 = cos(alpha_2) * l_radius;
         z_base2 = sin(alpha_2) * l_radius;
 
+        tx_2 = (float)i_slices / slices;
+
+        ty = (stacks - 1.0f) / stacks;
+
+
         y =  sin(M_PI / 2 - alphay) * (radius);
 
         
-        p1 = point(x_base1, y, z_base1, x_base1, y, z_base1);
-        p2 = point(0.0f, radius, 0.0f, 0.0f, radius, 0.0f);
-        p3 = point(x_base2, y, z_base2, x_base2, y, z_base2);
+        p1 = point(x_base1, y, z_base1, x_base1, y, z_base1,tx_1,ty);
+        p2 = point(0.0f, radius, 0.0f, 0.0f, radius, 0.0f,0 , 1);
+        p3 = point(x_base2, y, z_base2, x_base2, y, z_base2, tx_2, ty);
 
         ps.add_point(p1);
         ps.add_point(p2);
